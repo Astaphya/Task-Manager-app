42a1653fdeba2391f815864850a0e45878fa0f38
Astaphya <ender7tml@gmail.com>
2023-04-09 18:19:00 +0300

    Added task add feature

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,24 @@
+import {Task} from './Task'
+
+export const TASKS : Task[] = [
+    {
+        id:1,
+        text : 'Doctors Appointment',
+        day : 'May 5th at 2:30pm',
+        reminder:true,
+    },
+
+    {
+        id:2,
+        text : 'Watch Angular course',
+        day : 'May 6th at 8:30pm',
+        reminder:true,
+    },
+
+    {
+        id:3,
+        text : 'Watch some movies',
+        day : 'May 6th at 12:00am',
+        reminder:false,
+    },
+]
